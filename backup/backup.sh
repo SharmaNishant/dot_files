d00274b4db5495934e865a168910e2893d5d5816
Nishant Sharma <nishants1994@gmail.com>
2022-05-07 01:33:06 +0000

    take down guilfordranch.com

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,41 +1,16 @@
 backup_nextcloud () {
-
     sudo nextcloud.export -abcd
-
 }
-
 
 backup_external_nextcloud () {
-
     rsync -e 'ssh' -ah --info=progress2  --no-inc-recursive --delete-after cloudroot:/media/data/nextcloud_backups .
-
-}
-
-backup_lillis () {
-
-
-}
-
-backup_cloud_lillis () {
-
-}
-
-
-backup_external_lillis () {
-
-
-
 }
 
 backup_plex () {
-
     rsync -avh --info=progress2 --no-inc-recursive --delete-after ~/plex/ /media/data/plex/
-
 }
 
 backup_external_plex () {
-
     rsync -e 'ssh' -ahv --info=progress2 --no-inc-recursive --delete-after cloud:plex/ plex/
-
 }
 
